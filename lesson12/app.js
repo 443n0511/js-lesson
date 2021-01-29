6d669e1f842da47383bbb1a81a306ed0e3461a63
443n0511 <443n20070511@gmail.com>
2021-01-30 07:27:02 +0900

    Lesson１２の作成

---
+++ lesson12/app.js
@@ -0,0 +1,56 @@
+// 問題１２
+
+const div = document.getElementById("js-parent");
+const ul = document.createElement("ul");
+const getJsonUrl = "https://jsondata.okiba.me/v1/json/s7zm3210129115033";
+const button = document.getElementById("js-button");
+div.appendChild(ul);
+
+const botton = button.addEventListener('click', displayLodingImage, false);
+
+function displayLodingImage() {
+    fetch(getJsonUrl)
+        .then(response => response.json())
+        .then(data => { getJsonData(data.data); })
+        .catch((e) => { console.log(e) });
+
+    let image = document.createElement("img");
+    image.id = "lodingImage";
+    image.src = "../img/loading-circle.gif";    
+    div.appendChild(image);
+    button.classList.add("remove-button");
+    getJsonData();
+
+}
+
+
+function getJsonData(data) {
+    if (data) {
+        const jsonData = data;
+        setTimeout(() => {
+            let lodingImage = document.getElementById("lodingImage");
+            lodingImage.remove();
+            for (let i = 0; i < jsonData.length; i++) {
+                let li = document.createElement("li");
+                let anchor = document.createElement("a");
+                let image = document.createElement("img");
+                anchor.href = jsonData[i].to;
+                anchor.textContent = jsonData[i].text;
+                image.alt = jsonData[i].alt;
+                image.src = jsonData[i].img;
+                ul.appendChild(li);
+                li.appendChild(anchor);
+                anchor.appendChild(image);
+            }
+        }, 3000);
+
+
+    }
+}
+
+
+
+
+
+
+
